69dbe698f310d6b853c5cf2b2134abaa977e38ac
Guilherme dos Santos <guilherme@guisantos.com.br>
2019-10-03 13:40:01 -0300

    Nova Rota - /spots
    Criado a rota /spots (post) para cadastrar novos spots, mas ainda infuncional

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,7 +4,9 @@ const routes = express.Router();
 
 //Controllers
 const SessionController = require('./controller/SessionController');
+const SpotController = require('./controller/SpotController');
 
 routes.post('/session', SessionController.store);
+routes.post('/spots', SpotController.store);
 
 module.exports = routes;
