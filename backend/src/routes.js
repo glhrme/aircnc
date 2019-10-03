f477c50e1a5de6c01d22c76135b5f5440d0f1e06
Guilherme dos Santos <guilherme@guisantos.com.br>
2019-10-03 14:12:29 -0300

    Update de Rota | /spots (post)
    Rota /spots recebendo agora o multer/upload da imagem no campo thumbnail da requisição e retornando um console.log com os dados

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,12 +1,15 @@
 const express = require('express');
-
+const multer = require('multer');
+const uploadConfig = require('./config/upload');
 const routes = express.Router();
+
+const upload = multer(uploadConfig);
 
 //Controllers
 const SessionController = require('./controller/SessionController');
 const SpotController = require('./controller/SpotController');
 
 routes.post('/session', SessionController.store);
-routes.post('/spots', SpotController.store);
+routes.post('/spots', upload.single('thumbnail') ,SpotController.store);
 
 module.exports = routes;
