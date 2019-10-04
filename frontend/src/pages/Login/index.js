b6c1c4e3fec2bbdfc162e1915081636e25b01f51
Guilherme dos Santos <guilherme@guisantos.com.br>
2019-10-04 13:01:08 -0300

    Refactor Login
    Refatorado o index e tudo que continha login foi mandado para a index de login

---
+++ frontend/src/pages/Login/index.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import api from '../../services/api';
+
+export default function Login() {
+
+    //Set email atualiza o valor da variavel
+    //Real-time
+    const [email, setEmail] = useState('');
+
+    //Função responsável por enviar o submit
+    async function handleSubmit(event) {
+        //Não faça seu padrão de enviar pra outra tela
+        event.preventDefault();
+
+        const response = await api.post('/session', { email: email });
+        const { _id } = response.data;
+
+        localStorage.setItem('user', _id);
+
+    }
+
+    return (
+        <>
+            <p>
+                Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para a sua empresa
+            </p>
+
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="email">E-mail:</label>
+                <input
+                    id="email"
+                    type="email"
+                    placeholder="Seu melhor e-mail"
+                    onChange={event => setEmail(event.target.value)}
+                />
+                <button className="btn" type="submit">Entrar</button>
+            </form>
+        </>
+    );
+}
