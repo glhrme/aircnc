de5744c26b6a0c50d03a88b08fed8383e258f8a8
Guilherme dos Santos <guilherme@guisantos.com.br>
2019-10-04 11:14:30 -0300

    Formulário de Login
    Inicio ao formulário de login, onde está retornando o response em um console.log

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import api from './services/api';
 
 import logo from './assets/logo.svg';
 
 function App() {
+
+  //Set email atualiza o valor da variavel
+  //Real-time
+  const [ email, setEmail ] = useState('');
+
+  //Função responsável por enviar o submit
+  async function handleSubmit(event){
+    //Não faça seu padrão de enviar pra outra tela
+    event.preventDefault();
+
+    const response = await api.post('/sessions', { email:email });
+
+    console.log(response);
+
+  }
+
   return (
     <div className="container">
       <img src={logo} alt="Logo do AirCNC" />
@@ -12,12 +29,13 @@ function App() {
           Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para a sua empresa
         </p>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <label htmlFor="email">E-mail:</label>
           <input
             id="email"
             type="email"
             placeholder="Seu melhor e-mail"
+            onChange={event => setEmail(event.target.value)}
           />
           <button className="btn" type="submit">Entrar</button>
         </form>
