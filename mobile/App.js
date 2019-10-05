812ffb55722c01e7e70b0c4987f5317d1e387e33
Guilherme dos Santos <guilherme@guisantos.com.br>
2019-10-05 10:22:36 -0300

    Refatorado tela Inicial

--- mobile/App.js
+++ mobile/App.js
@@ -1,20 +1,8 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React from 'react';
+
+//Routes
+import Routes from './src/routes';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>AirCNC</Text>
-      <Text>Olá Mundo</Text>
-    </View>
-  );
+  return <Routes />
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
