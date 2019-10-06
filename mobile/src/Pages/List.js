233db4e51042fcc08feaa0278b0e3cd364760679
Guilherme dos Santos <guilherme@guisantos.com.br>
2019-10-06 09:06:01 -0300

    List iniciado
    List está obtendo os dados das tecnologias Async

--- mobile/src/Pages/List.js
+++ mobile/src/Pages/List.js
@@ -1,6 +1,30 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, AsyncStorage, Text, KeyboardAvoidingView, StyleSheet } from 'react-native';
 
 export default function List() {
-  return <View />;
+
+  const [ techs, setTechs ] = useState([]);
+
+  useEffect(()=>{
+    async function buscaTechs(){
+      const storagedTechs = await AsyncStorage.getItem('techs');
+      const techsArray = storagedTechs.split(',').map(tech => tech.trim());
+      setTechs(techsArray);
+    }
+    buscaTechs();
+  },[])
+
+  return (
+    <KeyboardAvoidingView style={styles.container}>
+      <Text>{techs}</Text>
+    </KeyboardAvoidingView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
