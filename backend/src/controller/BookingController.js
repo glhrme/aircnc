3f918faec6c3995759ebf53ecfcb108c0dcacf48
Guilherme dos Santos <guilherme@guisantos.com.br>
2019-10-09 07:08:23 -0300

    Solicitação de Reserva
    Ao receber uma solicitação de reserva do mobile, o back está verificando se o usuário dono do spot solicitado está logado na aplicação, e se estiver ele envia os dados para o front

--- backend/src/controller/BookingController.js
+++ backend/src/controller/BookingController.js
@@ -17,6 +17,14 @@ module.exports = {
             //Populando os relacionamentos criados no mongo
             await booking.populate('spot').populate('user').execPopulate();
 
+
+            const ownerSocket = req.connectedUsers[booking.spot.user];
+            
+            if(ownerSocket){
+                req.io.to(ownerSocket).emit('booking_request', booking);
+            }
+
+
             return res.status(200).json(booking);
 
         } catch (error) {
